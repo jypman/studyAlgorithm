8aed062941e33f08727518175226a896bcafae65
JINYOUNG PARK <asd0296@gmail.com>
2021-06-11 21:45:53 +0900

    특정 날짜에 맞는 요일 맞추기 알고리즘

--- 005.js
+++ 005.js
@@ -1,1 +1,11 @@
 // 2016년 - 연습문제
+const a = 5
+const b = 5
+
+function solution(a, b) {
+    const date = new Date(2016,a-1,b).getDay()
+    const getDay = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    return getDay[date];
+}
+
+console.log(solution(a, b))
