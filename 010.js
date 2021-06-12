b1a8f645db93f6cd7d773969ea0cb20efca0414d
JINYOUNG PARK <asd029600@gmail.com>
2021-06-12 17:01:41 +0900

    '문자열 내 마음대로 정렬하기 알고리즘 개선'

--- 010.js
+++ 010.js
@@ -3,17 +3,22 @@
 const strings = ["abce", "abcd", "cdx"]
 const n = 1
 
+// function solution(strings, n) {
+//     return strings.sort((a,b) => {
+//         if(a[n] > b[n]) return 1;
+//         else if(a[n] < b[n]) return -1;
+//         else if(a[n] === b[n]) {
+//             let i = 0;
+//             while (a[i] === b[i]) i++;
+//             if(a[i] > b[i]) return 1;
+//             else if(a[i] < b[i]) return -1;
+//         }
+//     })
+// }
 function solution(strings, n) {
-    return strings.sort((a,b) => {
-        if(a[n] > b[n]) return 1;
-        else if(a[n] < b[n]) return -1;
-        else if(a[n] === b[n]) {
-            let i = 0;
-            while (a[i] === b[i]) i++;
-            if(a[i] > b[i]) return 1;
-            else if(a[i] < b[i]) return -1;
-        }
-    })
+    return strings.sort((a, b) =>
+        a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n])
+    );
 }
 
 console.log('각 원소의 n번째 문자를 기준으로 오름차순 정렬한 배열 :',solution(strings, n));
