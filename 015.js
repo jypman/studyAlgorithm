895741434dee98bfa8a334509769bc8fde998517
JINYOUNG PARK <asd029600@gmail.com>
2021-06-13 03:52:50 +0900

    소수 찾기 반환값 로직 수정

--- 015.js
+++ 015.js
@@ -4,7 +4,6 @@ const n = 15;
 // 에라토스테네스의 체 알고리즘 적용
 function solution(n) {
     let primeNumberList = [];
-    let primeNumberCount = 0;
     for (let i = 0; i <= n; i++) {
         primeNumberList.push(i)
     }
@@ -14,11 +13,11 @@ function solution(n) {
             primeNumberList[j] = null
         }
     }
-    for (let i = 2; i <= n; i++) {
-        if(primeNumberList[i] !== null) ++primeNumberCount
-    }
     console.log('에라토스테네스의 체에 거르기 전 배열 :',primeNumberList)
-    return primeNumberCount;
+    primeNumberList = [...new Set(primeNumberList)]
+    return primeNumberList.indexOf(null) > -1
+        ? primeNumberList.length - 3
+        : primeNumberList.length - 2
 }
 
 console.log('소수의 갯수는? : ',solution(n))
