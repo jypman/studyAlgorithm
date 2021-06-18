bddae24ff53c5a7a37cb6711dc17334607b1ab99
JINYOUNG PARK <asd029600@gmail.com>
2021-06-19 03:39:53 +0900

    피보나치 수열 함수 구현

---
+++ dsAndAs/fibonacciSequence.js
@@ -0,0 +1,24 @@
+// 피보나치 수열
+
+// 재귀함수 이용
+function fibonacci(num) {
+    if(num === 1 || num === 2) return 1
+    return fibonacci( num - 2) + fibonacci(num - 1)
+}
+
+console.log('num 인자 값을 피보나치 수로 바꾸면?(재귀함수 이용)',fibonacci(4))
+
+// 단순 반복문 이용
+function FIBONACCI(num) {
+    let n1 = 1,
+        n2 = 1,
+        n = 1
+    for (let i = 3; i <= num; i++) {
+        n = n1 + n2
+        n1 = n2
+        n2 = n
+    }
+    return n
+}
+
+console.log('num 인자 값을 피보나치 수로 바꾸면?(단순 반복문)',FIBONACCI(4))
