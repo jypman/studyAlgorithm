b6e8ebda75712840c465df93cf7140a795b9d807
JINYOUNG PARK <asd029600@gmail.com>
2021-06-28 14:15:34 +0900

    라인 재팬 코딩 테스트 기출문제

--- line.js
+++ line.js
@@ -1,9 +1,42 @@
-// const fs = require('fs');
-// const input = fs.readFileSync(0,"utf-8").split('\n')
-// const input =
-//     '13:50:08.245 0.0'+'\n'+
-//     '13:50:11.123 4.0'+'\n'+
-//     '13:50:12.125 10.2'+'\n'+
-//     '13:50:13.100 8.7'+'\n'
-//
-// const arr = input.split('\n')
+// function main(argv) {
+//     argv.forEach((v, i) => console.log(`argv[${i}]: ${v}`));
+// }
+
+// main(process.argv.slice(2));
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    console.log(main(input))
+    rl.close()
+})
+    .on('close', () => {
+        process.exit()
+    });
+
+function exponential(number) {
+    let num = number
+    let answer = 1
+    for (let i = 0; i < num; i++) answer *= 2
+    return answer
+}
+
+function factorial(number) {
+    let num = number
+    let answer = 1
+    for (let i = num; i >= 1; i--) answer *= i
+    return answer
+}
+
+function main(argv) {
+    const number1 = parseInt(argv.split(' ')[0])
+    const number2 = parseInt(argv.split(' ')[1])
+    if(exponential(number1) > factorial(number2))
+        return 'Exponential'
+    else
+        return 'Factorial'
+}
