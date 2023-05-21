8136585548b2923f211ef239cc6775dcf5173775
jypman <asd029600@gmail.com>
2023-05-21 11:43:05 +0900

    fix : 큐의 dequeue 구현방법 수정

--- dataStructures/queue.js
+++ dataStructures/queue.js
@@ -1,29 +1,36 @@
 function Queue() {
-    let items = [];
+  let items = [];
+  let frontIndex = 0;
+  let rearIndex = 0;
 
-    this.enqueue = function (elem) {
-        items.push(elem)
-    }
+  this.enqueue = function (elem) {
+    items[rearIndex++] = elem;
+  };
 
-    this.dequeue = function () {
-        return items.shift()
-    }
+  // dequeue에서 shift()를 안 쓰는 이유
+  // shift()를 쓸 경우 o(n)의 선형시간으로 구현되기 때문에 o(1)의 시간복잡도를 갖는 큐와는 맞지 않으므로 shift()를 쓰지 않는다.
+  this.dequeue = function () {
+    const value = items[frontIndex];
+    delete items[frontIndex];
+    frontIndex += 1;
+    return value;
+  };
 
-    this.front = function () {
-        return items[0]
-    }
+  this.front = function () {
+    return items[frontIndex];
+  };
 
-    this.isEmpty = function () {
-        return items.length === 0
-    }
+  this.isEmpty = function () {
+    return items.every((val) => typeof val === "undefined");
+  };
 
-    this.size = function () {
-        return items.length
-    }
+  this.size = function () {
+    return rearIndex - frontIndex;
+  };
 
-    this.print = function () {
-        return items
-    }
+  this.print = function () {
+    return items;
+  };
 }
 
 export default Queue;
