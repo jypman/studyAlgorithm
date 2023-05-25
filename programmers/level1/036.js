a9e3bc763899c8815ef845a38f54918d417de25a
jypman <asd029600@gmail.com>
2023-05-25 12:46:26 +0900

    add : 최소직사각형 문제 풀이

--- programmers/level1/036.js
+++ programmers/level1/036.js
@@ -1,1 +1,41 @@
-// 최소 직사각형(완전탐색)
+// 최소 직사각형
+
+function solution(sizes) {
+  let maxWidth = 0;
+  let maxHeight = 0;
+  const sortSizes = sizes.map((elem) => elem.sort((a, b) => b - a));
+
+  sortSizes.forEach(([width, height]) => {
+    if (maxWidth < width) maxWidth = width;
+    if (maxHeight < height) maxHeight = height;
+  });
+
+  return maxWidth * maxHeight;
+}
+
+console.log(
+  solution([
+    [60, 50],
+    [30, 70],
+    [60, 30],
+    [80, 40],
+  ])
+);
+console.log(
+  solution([
+    [10, 7],
+    [12, 3],
+    [8, 15],
+    [14, 7],
+    [5, 15],
+  ])
+);
+console.log(
+  solution([
+    [14, 4],
+    [19, 6],
+    [6, 16],
+    [18, 7],
+    [7, 11],
+  ])
+);
