1f121f591b9002f84e5ffb880b446e5bf0093a48
jypman <asd029600@gmail.com>
2023-05-28 18:28:37 +0900

    add : 타겟 넘버 문제 풇이

---
+++ programmers/level2/020.js
@@ -0,0 +1,10 @@
+// 모음사전
+
+function solution(word) {
+  //
+}
+
+console.log(solution("AAAAE")); // 6
+console.log(solution("AAAE")); // 10
+console.log(solution("I")); // 1563
+console.log(solution("EIO")); // 1189
