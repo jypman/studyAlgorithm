c7bc39423cd6f363b81865ec22bae31d61d4a8e9
jypman <asd029600@gmail.com>
2023-05-31 19:31:15 +0900

    add : 모음사전 문제 풀이

--- programmers/level2/020.js
+++ programmers/level2/020.js
@@ -1,7 +1,23 @@
 // 모음사전
 
 function solution(word) {
-  //
+  const availableWords = "AEIOU";
+  const pickMaxCount = 5;
+  let wordArray = [];
+
+  function recursion(count, words) {
+    if (count === 0) {
+      wordArray.push(words);
+      return;
+    }
+    for (let i = 0; i <= pickMaxCount; i++) {
+      recursion(count - 1, `${words}${["", ...availableWords][i]}`);
+    }
+  }
+
+  recursion(pickMaxCount, "");
+
+  return [...new Set(wordArray)].sort().indexOf(word);
 }
 
 console.log(solution("AAAAE")); // 6
