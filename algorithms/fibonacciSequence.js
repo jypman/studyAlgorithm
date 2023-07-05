74e0d982c997d293df13bf077491d43f2082d8e6
jypman <asd029600@gmail.com>
2023-07-05 14:46:55 +0900

    add : 멀리뛰기 문제풀이

--- algorithms/fibonacciSequence.js
+++ algorithms/fibonacciSequence.js
@@ -11,14 +11,13 @@ console.log("num 인자 값을 피보나치 수로 바꾸면?(재귀함수 이�
 // 단순 반복문 이용
 function FIBONACCI(num) {
   let n1 = 1,
-    n2 = 1,
-    n = 1;
-  for (let i = 3; i <= num; i++) {
-    n = n1 + n2;
+    n2 = 1;
+  for (let i = 0; i < num - 2; i++) {
+    const temp = n1;
     n1 = n2;
-    n2 = n;
+    n2 = temp + n2;
   }
-  return n;
+  return n2;
 }
 
 console.log("num 인자 값을 피보나치 수로 바꾸면?(단순 반복문)", FIBONACCI(4));
