b3305d2aa2407d662342b90b0ca6fa0d797db72d
jypman <jypark150695@gmail.com>
2025-05-05 18:17:11 +0900

    add : 그리디 알고리즘 4-3-3 풀이

---
+++ fastCampus/greedy/4-3-3.js
@@ -0,0 +1,21 @@
+function solution(total) {
+  let TOTAL = total;
+  let startInt = 1;
+  let result = 0;
+
+  while (0 < TOTAL) {
+    TOTAL = TOTAL - startInt;
+
+    if (0 <= TOTAL) {
+      startInt++;
+      result++;
+    }
+  }
+
+  return result;
+}
+
+console.log(solution(200)); // 19
+console.log(solution(6)); // 3
+console.log(solution(5)); // 2
+console.log(solution(4)); // 2
