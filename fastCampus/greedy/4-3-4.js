ba93b9cbb2dee5d9259a0df9f31cdd1f5464b1c7
jypman <jypark150695@gmail.com>
2025-05-05 21:15:04 +0900

    add : 그리디 알고리즘 4-3-4 풀이

---
+++ fastCampus/greedy/4-3-4.js
@@ -0,0 +1,34 @@
+function solution(persons) {
+  const sortedPersons = persons.sort((a, b) => b[0] - a[0]);
+
+  let deletedPerson = 0;
+
+  sortedPersons.forEach(([_, b], index) => {
+    const isDelete = sortedPersons.slice(index).some(([_, B]) => b > B);
+    if (isDelete) deletedPerson++;
+  });
+
+  return persons.length - deletedPerson;
+}
+
+console.log(
+  solution([
+    [3, 2],
+    [1, 4],
+    [4, 1],
+    [2, 3],
+    [5, 5],
+  ])
+); // 4
+
+console.log(
+  solution([
+    [3, 6],
+    [7, 3],
+    [4, 2],
+    [1, 4],
+    [5, 7],
+    [2, 5],
+    [6, 1],
+  ])
+); // 3
