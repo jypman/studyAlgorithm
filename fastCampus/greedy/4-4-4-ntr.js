ca6f1753812327f73c120f3e4fd098c12a47141c
jypman <jypark150695@gmail.com>
2025-05-08 22:33:07 +0900

    add : 그리디 알고리즘 4-4-4 추가

---
+++ fastCampus/greedy/4-4-4-ntr.js
@@ -0,0 +1,12 @@
+/**
+ * @see {@link https://www.acmicpc.net/problem/9009}
+ * */
+
+function solution(num) {
+  //
+}
+
+console.log(solution(100));
+console.log(solution(200));
+console.log(solution(12345));
+console.log(solution(1003));
